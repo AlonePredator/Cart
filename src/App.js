289e12664df7cae7eecab090396047c90d1855e5
AlonePredator <sujityadav.2205@gmail.com>
2023-06-10 00:19:11 +0530

    added Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import CartItem from "./CartItem";
 import Cart from "./Cart";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
        {/* <h1>Cart Sujit</h1>  */}
+       <Navbar/>
        <Cart/>
     </div>
   );

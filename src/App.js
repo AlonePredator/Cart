81ff07015d338df99f14e680de8929ad67964b45
AlonePredator <sujityadav.2205@gmail.com>
2023-06-10 01:30:51 +0530

    Added Total Price

--- src/App.js
+++ src/App.js
@@ -12,28 +12,28 @@ class App extends React.Component {
             price: 999,
             title: 'Phone',
             qty: 4,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBob25lfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
             id: 1
             },
             {
             price: 99,
             title: 'Watch',
             qty: 10,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
             id: 2
             },
             {
             price: 9999,
             title: 'Laptop',
             qty: 15,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFwdG9wfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
             id: 3
             },
             {
             price: 599,
             title: 'Earphones',
             qty: 100,
-            img: '',
+            img: 'https://plus.unsplash.com/premium_photo-1668418188837-d40b734ed6d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZWFycGhvbmVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
             id: 4
             },
             ]
@@ -78,6 +78,14 @@ class App extends React.Component {
     })
     return count; 
   }
+  getCartTotal = () => {
+    const {products} = this.state;
+    let sum = 0;
+    products.forEach((product) => {
+      sum += product.qty*product.price;
+    })
+    return sum;
+  }
   render() {
     const {products} = this.state;
     return (
@@ -92,6 +100,7 @@ class App extends React.Component {
            onDecreaseQuantity = {this.handleDecreaseQuantity}
            onDeleteProduct = {this.handleDeleteProduct}
          />
+         <div style={{padding: 10, fontSize: 20}}>TOTAL: {this.getCartTotal( )}</div>
       </div>
     );
   }
